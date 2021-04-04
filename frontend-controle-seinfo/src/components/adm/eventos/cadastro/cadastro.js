957ede21570a7ac67808ce8ac0c04e26d50a89d5
thaise <thaisekaori@hotmail.com>
2021-04-04 16:17:05 -0300

    Revert "limpar lotes na ação cancelar eventos"
    This reverts commit a0f1a7972ccfb62b69f36b0511141d079bb6d2ef.

--- frontend-controle-seinfo/src/components/adm/eventos/cadastro/cadastro.js
+++ frontend-controle-seinfo/src/components/adm/eventos/cadastro/cadastro.js
@@ -79,7 +79,6 @@ export default {
       this.local_eve = "";
       this.select_status = "";
       this.descricao = "";
-      this.objeto_lote=[];
       this.obj_Resource=  {
         nome: "",
         data_ini: "",
